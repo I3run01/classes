8c6c3e9783dc6180de78d05b6faaf42967740d4b
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-08-28 21:55:36 -0300

    files deleted

--- Python/Python Automação/aulaSelenium.py
+++
@@ -1,7 +0,0 @@
-from xml.dom.minidom import Element
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-driver = webdriver.Chrome()
